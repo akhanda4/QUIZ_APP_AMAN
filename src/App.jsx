14977951bcb54ac13dc7259eadb2d8110777a664
akhanda4 <amanhanda420@gmail.com>
2020-02-04 10:45:41 +0530

    Added router packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
+import { Button, Navbar, NavDropdown, Nav, Form, FormControl } from 'react-bootstrap';
 import "./App.css";
 import Auxillary from "./components/auxillary/Auxillary.jsx";
+import { BrowserRouter } from 'react-router-dom';
 export default class App extends Component {
   constructor(props) {
     super(props);
@@ -8,9 +10,24 @@ export default class App extends Component {
 
   render() {
     return (
-      <Auxillary >
-        <p></p>
-      </Auxillary>
+      <BrowserRouter>
+        <Auxillary>
+          <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Navbar.Brand>QUIZ APP</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="mr-auto">
+              </Nav>
+              <Nav className="mr-auto">
+                <Button size="lg" variant="outline-light">Play Quiz</Button>
+              </Nav>
+              <Nav>
+                <Button size="lg" variant="outline-info">Login</Button>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </Auxillary>
+      </BrowserRouter>
     );
   }
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from "./App.jsx";
 import ReactDOM from "react-dom";
 import "./index.css";
