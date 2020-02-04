f2be928e3cf3fadb696bf7cc5b2aae6a262649cc
akhanda4 <amanhanda420@gmail.com>
2020-02-04 21:50:29 +0530

    dynamically rendering catagories

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import Auxillary from "./components/auxillary/Auxillary.jsx";
 import { BrowserRouter, Route, Redirect } from 'react-router-dom';
 import Login from "./components/layout/login.jsx";
 import Quizmaker from "./components/layout/quizmaker/quizmaker.jsx"
+import Catagories from "./components/layout/quizmaker/quizData/catagories.jsx";
+import Subcatagories from "./components/layout/quizmaker/quizData/subcatagories.jsx";
+import Questions from "./components/layout/quizmaker/quizData/questions.jsx";
 export default class App extends PureComponent {
   constructor(props) {
     super(props);
@@ -34,6 +37,9 @@ export default class App extends PureComponent {
       <BrowserRouter>
         <Auxillary>
           <Route path="/login" exact render={() => <Login authenticated={this.authenticated} isAuthenticated={this.state.isAuthenticated} />} />
+          <Route path="/admin/catagories" render={() => <Catagories />} />
+          <Route path="/admin/subcatagories" render={() => <Subcatagories />} />
+          <Route path="/admin/questions" render={() => <Questions />} />
           {this.state.isAuthenticated ? <Route path="/admin" exact render={() => <Quizmaker authenticated={this.authenticated} />} /> : <Redirect to='/login' />}
         </Auxillary>
       </BrowserRouter>
--- src/components/layout/quizmaker/quizmaker.jsx
+++ src/components/layout/quizmaker/quizmaker.jsx
@@ -1,23 +1,70 @@
-import React from 'react';
+import React, { PureComponent } from "react";
 import Tree from "./Tree.jsx";
 import Aggrid from "./Aggrid.jsx";
 import "../../../public/css/quizmaker.css";
-import Auxiliary from '../../auxillary/Auxillary.jsx';
+import Auxiliary from "../../auxillary/Auxillary.jsx";
 import Logout from "../logout.jsx";
-
-const quizmaker = (props) => {
+import { Nav, Navbar, Button } from "react-bootstrap";
+class quizmaker extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: ""
+    };
+  }
+  loadCatagories = () => {
+    this.setState({
+      active: "Catagories"
+    });
+  };
+  loadSubCatagories = () => {
+    this.setState({
+      active: "SubCatagories"
+    });
+  };
+  loadQuestions = () => {
+    this.setState({
+      active: "Questions"
+    });
+  };
+  render() {
+    const active = this.state.active;
+    var mnt = "";
+    if (this.state.active === "Catagories") {
+      mnt = (
+        <div className="row">
+          <div className="column">
+            <Tree />
+          </div>
+          <div className="column">
+            <Aggrid />
+          </div>
+        </div>
+      );
+    }
     return (
-        <Auxiliary>
-            <Logout authenticated={props.authenticated} />{/*//TODO:*/}
-            <div className="row">
-                <div className="column" >
-                    <Tree />
-                </div>
-                <div className="column" >
-                    <Aggrid />
-                </div>
-            </div>
-        </Auxiliary>
-    )
+      <Auxiliary>
+        <Logout authenticated={this.props.authenticated} />
+        <Nav fill variant="tabs">
+          <Nav.Item>
+            <Nav.Link eventKey="link-1" onClick={this.loadCatagories}>
+              Catagories
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link-2" onClick={this.loadSubCatagories}>
+              Subcatagories
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link-3" onClick={this.loadQuestions}>
+              Question
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+        {mnt}
+      </Auxiliary>
+    );
+  }
 }
 export default quizmaker;
